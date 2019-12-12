0e46028eb80b2b712fa2f7627ff2ffc49e847109
Mirzoeva <vselennaya314@gmail.com>
2019-12-12 18:42:20 +0300

    Scripted auto-commit on change (2019-12-12 18:42:20) by gitwatch.sh

--- src/main/java/lab6/ZooKeeper.java
+++ src/main/java/lab6/ZooKeeper.java
@@ -1,7 +1,9 @@
 package lab6;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -23,22 +25,33 @@ public class ZooKeeper {
     public static void main(String[] args) throws IOException {
         System.out.println(Constants.startMsg);
         Scanner in = new Scanner(System.in);
-        
+
         Constants constants = new Constants();
         constants.setPort(in.nextInt());
 
         ActorSystem system = ActorSystem.create("routes");
+        ActorRef storageActor = system.actorOf(Props.create(StorageActor::new));
+
+        ZooKeeper zooKeeper = new ZooKeeper(
+                
+
+        );
+
+
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        final AsyncHttpClient asyncHttpClient = asyncHttpClient();
-        final Tester tester = new Tester(asyncHttpClient, system, materializer);
+//
+//        final AsyncHttpClient asyncHttpClient = asyncHttpClient();
+//        final Tester tester = new Tester(asyncHttpClient, system, materializer);
+
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester.createRoute();
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(Constants.hostName, Constants.port),
                 materializer
         );
+
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
