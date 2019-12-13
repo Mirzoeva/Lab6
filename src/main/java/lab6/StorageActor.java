0e4ff6f92173af1f87fb9e0ce5fda0ec59da2939
Mirzoeva <vselennaya314@gmail.com>
2019-12-13 23:28:40 +0300

    Scripted auto-commit on change (2019-12-13 23:28:40) by gitwatch.sh

--- src/main/java/lab6/StorageActor.java
+++ src/main/java/lab6/StorageActor.java
@@ -5,16 +5,20 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import junit.framework.TestResult;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+
 import java.util.Map;
 import java.util.TreeMap;
 
 public class StorageActor  extends AbstractActor {
-    private final Map<UrlTest, Long> storage;
+    private final List<String> storage;
     private final Random random;
 
     public StorageActor(){
         this.random = new Random();
-        this.storage = new TreeMap<>();
+        this.storage = new ArrayList<>();
     }
     public static Props props(){
         return Props.create(StorageActor.class);
