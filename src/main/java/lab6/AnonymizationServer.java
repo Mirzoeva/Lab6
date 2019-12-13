ecfc954df75db8f4f79073b1267fc62e45f24db7
Mirzoeva <vselennaya314@gmail.com>
2019-12-13 23:49:54 +0300

    Scripted auto-commit on change (2019-12-13 23:49:54) by gitwatch.sh

--- src/main/java/lab6/AnonymizationServer.java
+++ src/main/java/lab6/AnonymizationServer.java
@@ -1,4 +1,30 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import org.asynchttpclient.AsyncHttpClient;
+
 public class AnonymizationServer {
+    private final AsyncHttpClient http;
+    private final ActorRef serversStorage;
+    private final ZooKeeper zooKeeper;
+
+    public AnonymizationServer(ActorRef serversStorage, AsyncHttpClient http, ZooKeeper zooKeeper){
+        this.http = http;
+        this.serversStorage = serversStorage;
+        this.zooKeeper = zooKeeper;
+    }
+
+    public Route createRoute(){
+        return route(
+                get( () -> parameter("url", url ->
+                                parameter( "count", count ->
+                                        handleGetWithUUrlCount(url, Integer.parseInt(count))
+                                )
+                        )
+
+                )
+        );
+    }
+
+    
 }
