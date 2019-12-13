384fd65e1dc4dec1cb56875aec553463aa88b74c
Mirzoeva <vselennaya314@gmail.com>
2019-12-14 00:45:11 +0300

    Scripted auto-commit on change (2019-12-14 00:45:11) by gitwatch.sh

--- src/main/java/lab6/DeleteServerMsg.java
+++ src/main/java/lab6/DeleteServerMsg.java
@@ -1,4 +1,13 @@
 package lab6;
 
 public class DeleteServerMsg {
+    private final String server;
+
+    public DeleteServerMsg(String server){
+        this.server = server;
+    }
+
+    public String getServer(){
+        return server;
+    }
 }
