a70a0e51c8c05244cd95b15e2a473cb2310f07bf
Mirzoeva <vselennaya314@gmail.com>
2019-12-14 01:39:47 +0300

    Scripted auto-commit on change (2019-12-14 01:39:47) by gitwatch.sh

--- src/main/java/lab6/ServersHandler.java
+++ src/main/java/lab6/ServersHandler.java
@@ -54,7 +54,4 @@ public class ServersHandler {
     public  void removeAllWatches() throws Exception{
         zooKeeper.removeAllWatches(serversPath, Watcher.WatcherType.Any, true);
     }
-
-
-
 }
