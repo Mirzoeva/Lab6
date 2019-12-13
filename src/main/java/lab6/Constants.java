711c8d345e921470a71fc8b8664ecca9589aef25
Mirzoeva <vselennaya314@gmail.com>
2019-12-13 23:58:37 +0300

    Scripted auto-commit on change (2019-12-13 23:58:37) by gitwatch.sh

--- src/main/java/lab6/Constants.java
+++ src/main/java/lab6/Constants.java
@@ -5,11 +5,7 @@ public class Constants {
     static String url = "testUrl";
     static String startMsg = "Start!";
     static String count = "count";
-    static int port;
+    static int port = 8080;
     static String hostName = "localhost";
-
-    public void setPort(int port){
-        port = port;
-    }
-
+    
 }
