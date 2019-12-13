5ecc08706c6b54be8fac798663910cbc02afaae7
Mirzoeva <vselennaya314@gmail.com>
2019-12-14 00:48:22 +0300

    Scripted auto-commit on change (2019-12-14 00:48:22) by gitwatch.sh

--- src/main/java/lab6/ReturnServerMsg.java
+++ src/main/java/lab6/ReturnServerMsg.java
@@ -1,4 +1,13 @@
 package lab6;
 
 public class ReturnServerMsg {
+    private final String server;
+
+    public ReturnServerMsg(String server){
+        this.server = server;
+    }
+
+    public String get(){
+        return server;
+    }
 }
