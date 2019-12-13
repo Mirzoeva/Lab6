3b08711a208341b96e6f943d83f078e1e0f89817
Mirzoeva <vselennaya314@gmail.com>
2019-12-14 00:47:14 +0300

    Scripted auto-commit on change (2019-12-14 00:47:14) by gitwatch.sh

--- src/main/java/lab6/PutServersMsg.java
+++ src/main/java/lab6/PutServersMsg.java
@@ -1,4 +1,15 @@
 package lab6;
 
+import java.util.List;
+
 public class PutServersMsg {
+    private final List<String> servers;
+
+    public  PutServersMsg(List<String> servers){
+        this.servers = servers;
+    }
+
+    public List<String> getServers(){
+        return servers;
+    }
 }
